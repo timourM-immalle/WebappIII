abe6165e07aee15b9c68c06489c27ac35077e65c
Timour Meeusen <timour.meeusen@immalle.eu>
2018-04-26 19:10:25 +0200

    begin PuntenIngeven.cshtml

---
+++ MyFirstWebAppII/Views/Home/PuntenIngeven.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstWebAppII.Views.Home
+{
+    public class PuntenIngevenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
